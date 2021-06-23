1a169e358290d3f48e75d8bdad01010ce3e4cacd
Erick Gómez <61529930+erickgl1003@users.noreply.github.com>
2021-06-23 17:04:54 -0500

    Initial commit

---
+++ app/src/main/java/com/example/flixster/MovieDetailsActivity.java
@@ -0,0 +1,43 @@
+package com.example.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.example.flixster.models.Movie;
+
+import org.parceler.Parcels;
+
+public class MovieDetailsActivity extends AppCompatActivity {
+
+    Movie movie;
+
+    TextView tvTitle;
+    TextView tvOverview;
+    RatingBar rbVoteAverage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_details);
+        // resolve the view objects
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvOverview = (TextView) findViewById(R.id.tvOverview);
+        rbVoteAverage = (RatingBar) findViewById(R.id.rbVoteAverage);
+
+        //unwrap movie passed via intent, using its simple name as key
+        movie = (Movie) Parcels.unwrap(getIntent().getParcelableExtra(Movie.class.getSimpleName()));
+        Log.d("MovieDetailsActivity", String.format("Showing details for '%s'", movie.getTitle()));
+
+        // set the title and overview
+        tvTitle.setText(movie.getTitle());
+        tvOverview.setText(movie.getOverview());
+
+        // vote average is 0..10, so we convert to 0..5 by dividing by 2
+        float voteAverage = movie.getVoteAverage().floatValue();
+        rbVoteAverage.setRating(voteAverage / 2.0f);
+    }
+}
---
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -0,0 +1,119 @@
+package com.example.flixster.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixster.MovieDetailsActivity;
+import com.example.flixster.R;
+import com.example.flixster.models.Movie;
+
+import org.jetbrains.annotations.NotNull;
+import org.parceler.Parcels;
+
+import java.util.List;
+
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies){
+        this.context = context;
+        this.movies = movies;
+
+    }
+
+    //Usually involves inflating a layout from XML and returning the holder
+    @NotNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter","onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+
+        return new ViewHolder(movieView);
+    }
+
+    //Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder(@NonNull @NotNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter","onBindViewHolder " + position);
+        //Get the movie at the passed in position
+        Movie movie = movies.get(position);
+
+        //Bind the movie data in the VH
+        holder.bind(movie);
+    }
+
+    //Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+
+        public ViewHolder(@NonNull @NotNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            itemView.setOnClickListener(this);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+
+            tvTitle.setTypeface(null, Typeface.BOLD);
+            tvOverview.setText(movie.getOverview());
+            tvOverview.setMovementMethod(new ScrollingMovementMethod());
+            String imageUrl;
+
+            //if phone is in landscape then use backdrop image else poster one
+            if(context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                imageUrl = movie.getBackdropPath();
+            } else{
+                imageUrl = movie.getPosterPath();
+            }
+
+            int radius = 30; // corner radius, higher value = more rounded
+            int margin = 10; // crop margin, set to 0 for corners with no crop
+            Glide.with(context).load(imageUrl).placeholder(R.drawable.flicks_movie_placeholder).centerCrop().transform(new RoundedCornersTransformation(radius, margin)).into(ivPoster);
+        }
+
+        @Override
+        public void onClick(View v) {
+            //Get item position
+            int position = getAdapterPosition();
+            // make sure the position is valid, i.e. actually exists in the view
+            if (position != RecyclerView.NO_POSITION) {
+                // get the movie at the position, this won't work if the class is static
+                Movie movie = movies.get(position);
+                // create intent for new activity
+                Intent intent = new Intent(context, MovieDetailsActivity.class);
+                // serialize the movie using parceler, use its short name as a key
+                intent.putExtra(Movie.class.getSimpleName(), Parcels.wrap(movie));
+                // show the activity
+                context.startActivity(intent);
+            }
+        }
+    }
+}
